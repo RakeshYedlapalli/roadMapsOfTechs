b68599d0005d79e0ff64e4cf7e7587807c4403be
RakeshYedlapalli <64858358+RakeshYedlapalli@users.noreply.github.com>
2024-01-12 10:33:08 +0800

    Create OnBlurrIssue.js

---
+++ OnBlurrIssue.js
@@ -0,0 +1,2 @@
+I am calling a javascript method from angular event from html file by passing the $event as input parameter, so I want to call the same 
+method from another javascript method by passing the $event, how can I do that, where would I get the $event object in javascript file?
