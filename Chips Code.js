6a57cd1dc8204427601331e388c72691bfb3cb16
RakeshYedlapalli <64858358+RakeshYedlapalli@users.noreply.github.com>
2024-01-17 15:41:28 +0800

    Update Chips Code.js

--- Chips Code.js
+++ Chips Code.js
@@ -1,31 +1,69 @@
-I have a below Angular JS code in Componenet.js file, where I have a mat-tab-group having 3 labels "General", "Tranches" and "Open in ClientLive", so recently I have migrated to Angular 15 from Angular 14, Before migration of Angular 
- 14, The <mat-tab label="GENERAL"> and <mat-tab label="TRANCHES"> were lefet aligned and <mat-tab label="Open in ClientLive"> is right aligned , However, after migration to Angular 15, they all aligned equally, like one after another 
-  with equal  padding, But I want to align the <mat-tab label="Open in ClientLive"> right and (<mat-tab label="GENERAL"> and <mat-tab label="TRANCHES">) are to be left, how to adjust my eithe CSS of HTML Code changes
+.p-list {
+  .p-row {
+    display: flex;
+    justify-content: space-between;
+    width: 100%;
+  
+    &.header {
+      font-weight: bold;
+      font-size: 14px;
+  
+      .p-col {
+        align-items: flex-end;
+        margin-bottom: 4px;
+      }
+  
+      .sort {
+        cursor: pointer;
+      }
+    }
+  
+    .p-col {
+      flex: 1;
+      display: flex;
+      align-items: center;
+      min-width: 0;
+      box-sizing: border-box;
+  
+      &.expander {
+        flex: 0 0 24px;
+      }
+  
+      &:not(.expander) {
+        padding-right: 10px;
+      }
 
-<mat-tab-group class="opportunity-view-tabs">
-                <mat-tab label="GENERAL">
-                    <app-general-tab-opportunity (sentToClientLiveChange)="onSentToClientLiveChange()" [isAddMode]="isAddMode" [opportunity]="opportunity" (opportunityChange)="onOpportunityChange($event)" #generalTabOpportunity></app-general-tab-opportunity>
-                </mat-tab>
-                <mat-tab label="TRANCHES">
-                    <app-tranche-tab></app-tranche-tab>
-                </mat-tab>
-                <mat-tab disabled>
-                    <ng-template mat-tab-label>
-                            <a [href]="clientLiveOpportunityUrl" mat-raised-button color="accent" target="_blank"
-                               rel="noreferrer"
-                               [disabled]="!opportunity?.sentToClientLive">Open in ClientLive
-                                <mat-icon>open_in_new</mat-icon>
-                            </a>
-                    </ng-template>
-                </mat-tab>
-            </mat-tab-group>
+      app-edit-in-place-form-field {
+        width: 100%;
+  
+        .not-editable-content {
+          overflow-wrap: break-word;
+        }
+      }
+    }
+  }
 
-and CSS Styles:
+  .mat-expansion-panel {
+    margin-bottom: 0.5rem !important;
 
+    .mat-expansion-panel-header {
+      cursor: default;
+      font-size: 14px;
+      padding: 0.5rem 0;
 
-::ng-deep .opportunity-view-tabs .mat-mdc-tab-label:last-child {
-  opacity: 1;
-  min-width: 20px;
-  margin-left: auto;
-  padding-right: 16px;
+      .mat-expansion-panel-header-title {
+        margin: 0;
+      }
+
+      .mat-expansion-indicator {
+        cursor: pointer;
+        display: flex;
+        margin: 0 8px !important;
+
+        &::after {
+            color:rgba(0, 0, 0, 0.54) !important;
+        }
+      }
+    }  
+  }
 }
